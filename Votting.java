9ef3317404222ea288b7cb9b043a5b8bbc4f96f2
Manishakachkure <30424885+Manishakachkure@users.noreply.github.com>
2017-08-09 15:13:49 +0530

    Rename Voting.java to Votting.java

---
+++ Votting.java
@@ -0,0 +1,15 @@
+ public class Votting
+{
+public static void main(String args[])
+{
+int a=24;
+if(a>=18)
+{
+System.out.println("you are illegable for voting");
+}
+else
+{
+System.out.println("you are not illegable for voting");
+}
+}
+}
